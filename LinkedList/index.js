6bb89ffb26fd4c5205f85c2affe744db5f1e919d
Henrynunez112 <henrynunez@pursuit.org>
2020-02-23 17:00:12 -0500

    just started the res

---
+++ LinkedList/index.js
@@ -0,0 +1,79 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(){
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+    push(value){
+      const node =  new Node(value);
+      if(this.head === null){
+        this.head = node;
+        this.tail = node;
+      }else{
+        node.next = this.head
+        this.head = node
+      }
+      this.length ++
+    }
+  
+    append(value){
+      const newNode = new Node(value);
+      if(!this.head){
+        this.head = newNode;
+        this.tail = newNode;
+        return
+      }else{
+          this.tail.next = newNode;
+          this.tail = newNode;
+      }
+      this.length++
+    }
+    print(){
+      let display = "-> ";
+      let pointer = this.head;
+      while(pointer){
+        display += pointer.value + "-> ";
+        pointer += pointer.next;
+      }
+      display += 'null'
+      console.log(display)
+    }
+    nodeAt(index){
+      if(index >= this.length){
+        throw Error("Invalid index");
+      }
+      let counter = index;
+      let pointer = this.head;
+      while(counter > 0){
+        counter--;
+        pointer = pointer.next;
+      }
+      return pointer.value
+    }
+    pop(){
+  
+      if(this.head === null){
+        return null
+      }
+      const tail = this.tail;
+  
+      if(this.head === this.tail){
+        this.head = null;
+        this.tail = null;
+      }else{
+        const pointer = this.head;
+        while(pointer.next !== this.tail){
+          pointer = pointer.next;
+        }
+        this.tail = pointer;
+        pointer.next = null;
+      }
+    }
+  }
